810a685976b6ea0f455ac54474b85c642be5f20e
kristinhakonardottir <kristinhelga0@gmail.com>
2026-04-22 23:37:04 +0000

    Wed, Apr 22, 2026, 7:37 PM -04:00

--- syllabus_transformer.py
+++ syllabus_transformer.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 import json
 
-API_KEY = "AIzaSyABC9koWOBHi00"
+API_KEY = "AIzaSyABC9koWOBHi00-aFW5rAbmp7snwnG6rFs"
 genai.configure(api_key=API_KEY)
 
 def extract_and_save_weights(syllabus_path):
